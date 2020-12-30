94e3868af93ef4fafbdc155f391bf02f8fb0cc98
Alex Ghiculescu <alex@tanda.co>
2020-12-30 16:23:10 -0600

    Use syntax that works with Sprockets 3 and 4
    Fixes https://github.com/Jakenberg/rails-webp/issues/16

--- lib/rails/webp/railtie.rb
+++ lib/rails/webp/railtie.rb
@@ -7,10 +7,10 @@ module Rails
 
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
-          env.register_mime_type 'image/jpeg', '.jpeg'
+          env.register_mime_type 'image/jpeg', extensions: ['.jpeg']
           env.register_postprocessor 'image/jpeg', PostProcessor
 
-          env.register_mime_type 'image/png', '.png'
+          env.register_mime_type 'image/png', extensions: ['.png']
           env.register_postprocessor 'image/png', PostProcessor
         end
       end
